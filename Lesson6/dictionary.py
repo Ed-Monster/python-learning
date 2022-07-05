dc8031632b9b9f5e79f843def15938897e9711b2
Ed-Monster <ed_zzf@163.com>
2022-07-05 16:44:15 +0800

    Lesson6

---
+++ Lesson6/dictionary.py
@@ -0,0 +1,11 @@
+favorite_languages = {
+    'jen': 'c',
+    'sara': 'python',
+    'sakamoto': 'java',
+    'edward': 'python',
+}
+
+# loop through the dictionary
+for key, value in favorite_languages.items():
+    print(f'{key.title()} likes {value.title()}.')
+
